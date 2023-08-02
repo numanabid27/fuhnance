15ea8b903ea270934d1a1ad3af59b07c153a57ec
numanabid <89048775+webdevelive@users.noreply.github.com>
2023-08-03 03:01:09 +0500

    set slider

--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -5,21 +5,43 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 export const Testimonial = () => {
-console.log(Data.clientsData)
+    console.log(Data.clientsData)
     return (
         <>
             <section className='bg-dark py-20'>
-                <div className='container m-auto'>
+                <div>
                     <Heading heading="Testimonial" />
 
-                    <div className='bg-dark-color'>
-                        <Swiper
-                            spaceBetween={50}
-                            slidesPerView={1}
-                            onSwiper={(swiper) => console.log(swiper)}
-                        >
-                            <SwiperSlide>Slide 1</SwiperSlide>
-                        </Swiper>
+                    <div className='bg-dark-color relative py-8 px-5'>
+                        <div className='container m-auto'>
+                            <Swiper
+                                spaceBetween={50}
+                                slidesPerView={1}
+                                onSwiper={(swiper) => console.log(swiper)}
+                            >
+                                {
+                                    Data.clientsData.map(({ clientName, designation, desc, clinetImg }, i) => {
+                                        return (
+                                            <div>
+                                                <SwiperSlide key={i.toString()}>
+                                                    <div className='flex items-center justify-between'>
+                                                        <div className='p-5 w-1/3 bg-dark'>
+                                                            <h4 className='text-white font-extrabold'>{clientName}</h4>
+                                                            <p className='text-[#C4C4C4] font-extralight text-[12px]'>{designation}</p>
+                                                            <h6 className='text-[#C4C4C4] font-extralight text-[14px]'>{desc}</h6>
+                                                            <img src="./images/star.png" alt="" className='w-36' />
+                                                        </div>
+                                                        <div className=''>
+                                                            <img src="./images/client.png" alt="" className='client_img'/>
+                                                        </div>
+                                                    </div>
+                                                </SwiperSlide>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </Swiper>
+                        </div>
                     </div>
                 </div>
             </section>
--- src/data/Data.js
+++ src/data/Data.js
@@ -1,22 +1,22 @@
 export const Data = {
     "clientsData" : [
         {
-            "clientName" : "",
-            "designation":"",
-            "desc":"",
-            "clinetImg" : ""
+            "clientName" : "Nick Weimann",
+            "designation":"District Directives Representative",
+            "desc":"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+            "clinetImg" : "./images/client.png"
         },
         {
-            "clientName" : "",
-            "designation":"",
-            "desc":"",
-            "clinetImg" : ""
+            "clientName" : "Nick Weimann",
+            "designation":"District Directives Representative",
+            "desc":"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+            "clinetImg" : "./images/client.png"
         },
         {
-            "clientName" : "",
-            "designation":"",
-            "desc":"",
-            "clinetImg" : ""
-        },
+            "clientName" : "Nick Weimann",
+            "designation":"District Directives Representative",
+            "desc":"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+            "clinetImg" : "./images/client.png"
+        }
     ]
 }
