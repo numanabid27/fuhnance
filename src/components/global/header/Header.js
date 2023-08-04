c41ee30dc3a0d4e3397f4d1da96f271216a5ad75
numanabid27 <numanabid27@gmail.com>
2023-08-04 05:28:35 -0700

    update branch

--- src/components/global/header/Header.js
+++ src/components/global/header/Header.js
@@ -101,7 +101,7 @@ const NavList = () => {
         <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 md:px-0 px-4">
             <li className="py-3 px-1 font-normal">
                 <Link to="/" className={`${chnageSyleNav} flex items-center font-normal`}>
-                    Home
+                    Homes
                 </Link>
             </li>
             <li className="py-3 px-1 font-normal item-list">
