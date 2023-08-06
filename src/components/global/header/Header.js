267e0a5356b6794cdb465e6f863b799905888a0f
numanabid <89048775+webdevelive@users.noreply.github.com>
2023-08-07 02:10:54 +0500

    pending navigation

--- src/components/global/header/Header.js
+++ src/components/global/header/Header.js
@@ -10,11 +10,9 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     const [openNav, setOpenNav] = React.useState(false);
-    const [isOpen, setIsOpen] = useState(false);
     const location = useLocation();
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-    }
+    const [isOpen, setIsOpen] = useState(false);
+
     React.useEffect(() => {
         window.addEventListener(
             "resize",
@@ -22,77 +20,6 @@ export default function Header() {
         );
         setOpenNav(false)
     }, [location.pathname]);
-
-    return (
-        <header>
-            <Navbar className={`${location.pathname === "/platform" ? "bg-white" : "bg-darkblue"}`}>
-                <div className="container mx-auto flex items-center justify-between text-blue-gray-900 side-padding">
-                    <div>
-                        {location.pathname === "/platform" ?
-                            <Link to="/" className="text-color-secondary md:text-[30px] font-semibold text-[20px]">
-                                <img src="./images/palform-logo.png" alt="" className="w-3/4 md:w-full" />
-                            </Link>
-                            :
-                            <Link to="/" className="text-color-secondary md:text-[30px] font-semibold text-[20px]">
-                                <img src="./images/getting-logo.png" alt="" className="w-3/4 md:w-full" />
-                            </Link>
-                        }
-                    </div>
-                    <div className="hidden lg:block">{<NavList />}</div>
-
-                    <IconButton
-                        variant="text"
-                        className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-                        ripple={false}
-                        onClick={() => setOpenNav(!openNav)}
-                    >
-                        {openNav ? (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill='none'
-                                className="h-6 w-6"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                style={{ color: location.pathname === "/platform" && "black" }}
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        ) : (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
-                                fill='none'
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                style={{ color: location.pathname === "/platform" && "black" }}
-
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        )}
-                    </IconButton>
-                </div>
-                <MobileNav open={openNav}>
-                    <div className="container mx-auto">
-                        {<NavList handleClick={handleClick} isOpen={isOpen} />}
-                    </div>
-                </MobileNav>
-            </Navbar>
-        </header>
-    );
-}
-
-
-const NavList = () => {
 
     const data = [
         {
@@ -149,52 +76,142 @@ const NavList = () => {
         },
     ]
 
+    return (
+        <header>
+            <Navbar className={`${location.pathname === "/platform" ? "bg-white" : "bg-darkblue"}`}>
+                <div className="container mx-auto flex items-center justify-between text-blue-gray-900 side-padding">
+                    <div>
+                        {location.pathname === "/platform" ?
+                            <Link to="/" className="text-color-secondary md:text-[30px] font-semibold text-[20px]">
+                                <img src="./images/palform-logo.png" alt="" className="w-3/4 md:w-full" />
+                            </Link>
+                            :
+                            <Link to="/" className="text-color-secondary md:text-[30px] font-semibold text-[20px]">
+                                <img src="./images/getting-logo.png" alt="" className="w-3/4 md:w-full" />
+                            </Link>
+                        }
+                    </div>
+                    <div className="hidden lg:block">
+                        {
+                            data.map((t, i) => {
+                                return (
+                                    <NavList
+                                        key={i.toString()}
+                                        title={t.title}
+                                        sublinks={t.sublinks}
+                                        subtitle={t.subtitle}
+                                        sublink={t.sublink}
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+
+                    <IconButton
+                        variant="text"
+                        className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                        ripple={false}
+                        onClick={() => setOpenNav(!openNav)}
+                    >
+                        {openNav ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill='none'
+                                className="h-6 w-6"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                style={{ color: location.pathname === "/platform" && "black" }}
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill='none'
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                style={{ color: location.pathname === "/platform" && "black" }}
+
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                            </svg>
+                        )}
+                    </IconButton>
+                </div>
+                <MobileNav open={openNav}>
+                    <div className="container mx-auto">
+                        {
+                            data.map((t, i) => {
+                                return (
+                                    <NavList
+                                        key={i.toString()}
+                                        isOpen={isOpen === i ? true : false}
+                                        title={t.title}
+                                        sublinks={t.sublinks}
+                                        subtitle={t.subtitle}
+                                        sublink={t.sublink}
+                                        handleClick={() => {
+                                            if (isOpen !== i) {
+                                                return setIsOpen(i)
+                                            }
+                                            setIsOpen(false)
+                                        }}
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                </MobileNav>
+            </Navbar>
+        </header>
+    );
+}
+
+
+const NavList = ({ id, title, link, sublinks, sublink, subtitle, isOpen, handleClick }) => {
+
     const location = useLocation();
     const chnageSyleNav = location.pathname === "/platform" ? "text-[#000814]" : "text-white";
-    
-    
+
     return (
         <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 md:px-0 px-4">
 
-            {
-                data.map(({ id, title, link, sublinks }) => {
-                    return (
-                        <li className="py-3 px-1 font-normal item-list" key={id}>
-                            <Link to={link} className={`${chnageSyleNav} font-normal flex items-center `} >
-                                {title}
-                                {
-                                    sublinks && <span className="ml-1">
-                                        <IoIosArrowDown />
-                                    </span>
-                                }
-
-                            </Link>
-                            {
-                                sublinks &&
-                                <ul className="item-menu">
-                                    {
-                                        sublinks?.map(({ subtitle, sublink }) => {
-                                            return (
-                                                <li className="pb-1">
-                                                    <Link to={sublink} className={`${chnageSyleNav} flex items-center font-normal`}>
-                                                        {subtitle}
-                                                    </Link>
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                </ul>
-                            }
-
-                        </li>
-                    )
-                })
-            }
-            <li className="py-3 px-1 font-normal header_list">
+            <li className="py-3 px-1 font-normal item-list" onClick={handleClick}>
+                <Link to={link} className={`${chnageSyleNav} font-normal flex items-center `} >
+                    {title}
+                    <span className="ml-1">
+                        <IoIosArrowDown />
+                    </span>
+                </Link>
+                <ul className={`${isOpen ? "block" : "hidden"}`}>
+                    {
+                        sublinks?.map(({ sublink, subtitle }, i) => {
+                            return (
+                                <li className="pb-1" key={i.toString()}>
+                                    <Link to={sublink} className={`${chnageSyleNav} flex items-center font-normal`}>
+                                        {subtitle}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </li>
+            {/* <li className="py-3 px-1 font-normal header_list">
                 <Link to="/company" className="flex items-center font-normal text-white">
                     Get Started
                 </Link>
-            </li>
+            </li> */}
         </ul>
     )
 }
