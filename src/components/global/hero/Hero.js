e916a4ea22d4102aa79360eca9f23d5d550783aa
numanabid27 <numanabid27@gmail.com>
2023-08-09 06:05:34 -0700

    update table container

--- src/components/global/hero/Hero.js
+++ src/components/global/hero/Hero.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { useDimensions } from "../../../hooks/use-dimensions";
 
 export const Hero = ({ bg, heading, para, height }) => {
-
+  const { width } = useDimensions();
   return (
     <>
-      <section style={{ backgroundImage: bg }} className='bg-cover bg-no-repeat bg-center'>
+      <section style={{ backgroundImage: bg, backgroundSize:width && width < 1200 ? "cover" : "100% 100%" }} className='bg-no-repeat bg-center'>
         <div className={`${height ? "lg:h-screen h-[80vh]" : "h-96"} text-center container m-auto side-padding`}>
           <div className={`${height ? "h-full" : "h-full"} flex justify-center items-center  flex-col`}>
             <h1 className='text-white xl:text-xl lg:text-2xl text-xs md:text-lg font-semibold leading-[1.2] max-w-[1051px]'>{heading}</h1>
